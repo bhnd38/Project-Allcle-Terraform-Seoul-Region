124277203823ee888c87d9d2113925ef1f07a3ae
bhnd38 <ebonearl@naver.com>
2024-09-19 12:11:21 +0900

    Rename and parted tf files

---
+++ local/security_groups.tf
@@ -0,0 +1,54 @@
+# Bastion 보안 그룹 생성
+resource "aws_security_group" "bastion_sg" {
+  vpc_id = aws_vpc.allcle_vpc.id
+  name   = "Bastion-SG"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # 실제 운영 환경에서는 특정 IP 범위로 제한해야 합니다.
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Bastion-SG"
+  }
+}
+
+# ALB 보안 그룹
+resource "aws_security_group" "alb_sg" {
+  vpc_id = aws_vpc.allcle_vpc.id
+  name   = "ALB-SG"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ALB-SG"
+  }
+}
